0ed7af0002d23b90307cac8aa76af966024bc483
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2023-02-17 11:29:20 +0500

    portfolio cards changet

--- src/components/Pages/Main/components/Cards/data.js
+++ src/components/Pages/Main/components/Cards/data.js
@@ -1,62 +1,62 @@
 export const portfolio = [
     {
         id: 0,
-        title: 'Agro bank',
-        desc: 'LLC OKS',
+        title: 'Inbound ',
+        desc: 'LLC Getter',
         image: '/portfolio/1.png'
     },
     {
         id: 1,
-        title: 'Agro bank',
-        desc: 'LLC OKS',
+        title: 'Simple Tok',
+        desc: 'LLC Getter',
         image: '/portfolio/2.png'
     },
     {
         id: 2,
-        title: 'Agro bank',
-        desc: 'LLC OKS',
+        title: 'Kassa nazorati',
+        desc: 'LLC Getter',
         image: '/portfolio/3.png'
     },
     {
         id: 3,
-        title: 'Alpomish Learning Center',
-        desc: 'LLC OKS',
+        title: 'Nayuta',
+        desc: 'LLC Getter',
         image: '/portfolio/4.png'
     },
     {
         id: 4,
-        title: 'Safir',
-        desc: 'LLC OKS',
+        title: 'Mebel',
+        desc: 'LLC Getter',
         image: '/portfolio/5.png'
     },
     {
         id: 5,
-        title: 'Ассоциация Гастрономического Туризма',
-        desc: 'LLC OKS',
+        title: 'Student Hunter',
+        desc: 'LLC Getter',
         image: '/portfolio/6.png'
     },
     {
         id: 6,
-        title: 'Касса контроль',
-        desc: 'LLC OKS',
+        title: 'Uz Kassa',
+        desc: 'LLC Getter',
         image: '/portfolio/7.png'
     },
     {
         id: 7,
-        title: 'Swiss Dental',
-        desc: 'LLC OKS',
+        title: 'Sakura',
+        desc: 'LLC Getter',
         image: '/portfolio/8.png'
     },
     {
         id: 8,
-        title: 'Gastromahalla',
-        desc: 'LLC OKS',
+        title: 'Makler',
+        desc: 'LLC Getter',
         image: '/portfolio/9.png'
     },
     {
         id: 9,
-        title: 'Agro bank',
-        desc: 'LLC OKS',
+        title: 'Pixer',
+        desc: 'LLC Getter',
         image: '/portfolio/10.png'
     },
 ]

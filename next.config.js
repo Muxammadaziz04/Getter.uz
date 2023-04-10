feb0daf2ee1178bf1d3a2c241ce1fbbba6810e1f
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2023-04-10 12:45:10 +0500

    disabled eslint

--- next.config.js
+++ next.config.js
@@ -9,11 +9,11 @@ const nextConfig = {
   images: {
     domains:["localhost"]
   },
-  extends: "next",
-  rules: {
-    "react/no-unescaped-entities": "off",
-    "@next/next/no-page-custom-font": "off"
-  }
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig

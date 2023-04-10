5da2d9068744c0c1231c95801db94da41e1ba8b6
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2023-04-10 12:43:16 +0500

    com

--- next.config.js
+++ next.config.js
@@ -8,6 +8,11 @@ const nextConfig = {
   },
   images: {
     domains:["localhost"]
+  },
+  extends: "next",
+  rules: {
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off"
   }
 }
 
